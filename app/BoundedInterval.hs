538dffd31617dc89ab32d7b1f1ffc621ca865284
Christian Micheletti <christian.micheletti@studenti.unipd.it>
2023-10-24 20:12:17 +0000

    init reflection per bound

--- app/BoundedInterval.hs
+++ app/BoundedInterval.hs
@@ -1,2 +1,39 @@
+{-# LANGUAGE FlexibleContexts #-}
 module BoundedInterval where
+import Data.Reflection (Reifies (reflect), reify)
+import Data.Proxy (Proxy)
+import Interval (Interval(Range))
+import AI
+import Algebra.Lattice
 
+mkBound :: (Reifies s (Rational, Rational)) => Proxy s -> Interval
+mkBound bound = uncurry Range (reflect bound)
+
+newtype BoundedInterval s r = BI Interval
+    deriving (Show, Eq)
+
+instance Lattice (BoundedInterval s r) where
+  (BI i) \/ (BI j) = BI $ i \/ j
+  (BI i) /\ (BI j) = BI $ i /\ j
+
+instance BoundedMeetSemiLattice (BoundedInterval s r) where 
+  top = BI top
+
+instance BoundedJoinSemiLattice (BoundedInterval s r) where
+  bottom = BI bottom
+
+
+instance (Reifies s r) => AI (BoundedInterval s r) where
+  abstractA = error "Not implemented"
+  abstractB = error "Not implemented"
+
+
+{-
+>>>example1
+Range (3 % 1) (3 % 1)
+-}
+example1 :: Interval
+example1 = reify (3, 3) mkBound
+
+intervalAnalize :: (Reifies s r) => s -> While -> AState (BoundedInterval s r)
+intervalAnalize bound = analyze
